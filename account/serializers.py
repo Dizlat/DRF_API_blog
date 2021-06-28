31620980d3411da21b3c869e3aac9ef03dc6313c
Dizlat <dizlat.ur@gmail.com>
2021-06-28 14:55:45 +0600

    account register

---
+++ account/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=6, required=True)
+    password_confirm = serializers.CharField(min_length=6, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password_confirm', 'first_name', 'last_name']
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Такой пользователь уже зарегистрирован')
+        return email
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirm = attrs.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.create_activation_code()
+        user.send_activation_email(user.email, user.activation_code)
+        return user
+
+
