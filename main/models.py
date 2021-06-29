14deb14d6e079475afe58233cee7130bbdcdcf33
Dizlat <dizlat.ur@gmail.com>
2021-06-29 12:24:18 +0600

    main models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='posts', blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+
+
